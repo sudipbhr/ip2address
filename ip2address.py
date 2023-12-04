725f27aa7d36db8a81a92ffe796227f6dd307738
Sudip Bhandari <50023508+sudipbhr@users.noreply.github.com>
2023-12-04 23:34:07 +0545

    initial commit

---
+++ ip2address.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    return response
+
+
+print(get_location())
